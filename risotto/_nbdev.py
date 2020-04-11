da02d3f18fa6aaaf9b9a477d94aacfb419534e6d
Luis Marti <lmarti@xyz>
2020-04-10 22:38:53 -0400

    moving

--- risotto/_nbdev.py
+++ risotto/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"hello": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://lmarti.github.io/risotto/"
 
