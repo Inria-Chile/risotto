0ad1ac344dbc65f71c6bdca112243c39589e5ce1
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-05-20 17:17:15 -0400

    Add Heroku files

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python scripts/prerelease.py
+web: voila --port=$PORT --no-browser --template=material --enable_nbextensions=True 06_GUI.ipynb
---
+++ scripts/prerelease.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+from nbdev.export import notebook2script
+
+CORD19_DATASET_FOLDER = Path("./datasets/CORD-19-research-challenge")
+
+sys.path.append(".")
+
+def main():
+    notebook2script()
+
+    from risotto.downloader import download_cord19_dataset
+    from risotto.artifacts import build_artifacts
+
+    download_cord19_dataset()
+    _, _, _ = build_artifacts(CORD19_DATASET_FOLDER)
+
+if __name__ == "__main__":
+    main()
