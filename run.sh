3dfded373156f4815648912bfcd8c27ecafad1af
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-06-01 16:35:11 -0400

    SSL support (#11)
    * Add lets-nginx
    
    * Expose risotto port
    
    * lets-encrypt service depends on risotto

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Preprocessing data"
 python ./scripts/preprocess.py
 
 echo "Running voila!!"
-voila --port=80 --no-browser --enable_nbextensions=True 06_GUI.ipynb --VoilaConfiguration.file_whitelist="['.*\.(png|jpg|gif|svg|mp4|avi|ogg|ttf)']"
+voila --port="$PORT" --no-browser --enable_nbextensions=True 06_GUI.ipynb --VoilaConfiguration.file_whitelist="['.*\.(png|jpg|gif|svg|mp4|avi|ogg|ttf)']"
