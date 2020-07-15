d7d83d4061ab4abfb101beee27864f4f8e5d1664
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-07-15 13:12:22 -0400

    Serve zero-shot in /

--- streamlit_apps/zero_shot_run.sh
+++ streamlit_apps/zero_shot_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-streamlit run streamlit_apps/zero_shot.py --server.port 8000 --server.baseUrlPath "/zero-shot"
+streamlit run streamlit_apps/zero_shot.py --server.port 8000 --server.baseUrlPath "/"
